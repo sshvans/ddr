aa0748b421a33377bac5b1763c595da00f4c3d10
mdmiller <mdmiller@amazon.com>
2017-08-14 16:42:10 -0700

    added captures_per_second param and camera warmup time

--- ddr_raspi/ddr_camera.py
+++ ddr_raspi/ddr_camera.py
@@ -15,17 +15,23 @@ with open("ddr_camera.props", 'r') as propsfile:
 
 bucket = props.get('s3_bucket')
 rotation = props.get('rotation', 0)
+captures_per_second = props.get('captures_per_second', 10)
 
 camera = PiCamera()
 camera.rotation = rotation
+
 camera.start_preview()
+
+# camera warmup
+sleep(2)
+
 print 'camera started'
 
 for i in range(5):
-    sleep(0.5)
     filename='image%s.jpg' % i
     pathname='/home/pi/images/' + filename
     camera.capture(pathname)
+    sleep(1/captures_per_second)
     data = open(pathname, 'rb')
     s3.upload_file(pathname, bucket, filename)
 
