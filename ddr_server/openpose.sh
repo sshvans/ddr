56c95a4f7a1f02681b0540e85a237572606ebe76
vsnyc <vsnyc@noreply.github.com>
2017-09-01 13:08:20 -0400

    Disable image generation to save time.

--- ddr_server/openpose.sh
+++ ddr_server/openpose.sh
@@ -4,9 +4,9 @@ if [ "$(ls -A ~/images/*.jpg)" ]; then
   cd ~/openpose/
   ./build/examples/openpose/openpose.bin --image_dir ~/images-processed --write_keypoint_json ~/json/ --no_display
   # Disable rendered image generation for faster processing
-  ./build/examples/openpose/openpose.bin --image_dir ~/images-processed --write_images ~/rendered/ --no_display
+  #./build/examples/openpose/openpose.bin --image_dir ~/images-processed --write_images ~/rendered/ --no_display
   aws s3 sync ~/json/ s3://ddr-raspi-bucket-1uttsilsw5opt/json/
-  aws s3 sync ~/rendered/ s3://ddr-raspi-bucket-1uttsilsw5opt/rendered/
+  #aws s3 sync ~/rendered/ s3://ddr-raspi-bucket-1uttsilsw5opt/rendered/
   mv ~/images-processed/*.jpg ~/archived/
   mv ~/json/*.json ~/archived/
   mv ~/rendered/*.png ~/archived/
