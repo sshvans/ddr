1b0dc97bf9de3d02297e3e19cc446184f3914170
vsnyc <vsnyc@noreply.github.com>
2017-09-12 17:31:41 -0400

    Fixed api_url parsing

--- deploy_webapp.sh
+++ deploy_webapp.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
 S3_BUCKET=$( cat ddr_config.props | grep s3_bucket | cut -d':' -f2 | tr -d ' ' )
-API_URL=$( cat ddr_config.props | grep api_url | cut -d':' -f2 | tr -d ' ' )
+echo ${S3_BUCKET}
+API_URL=$( cat ddr_config.props | grep api_url | cut -d':' -f2- | tr -d ' ' )
+echo ${API_URL}
 S3_WEBAPP_BUCKET=$( cat ddr_config.props | grep s3_webapp_bucket | cut -d':' -f2 | tr -d ' ' )
+echo ${S3_WEBAPP_BUCKET}
+
 cd scoreboard-web-app
+
 for html_file in *.html;
 do
 sed -i '' "s/S3_BUCKET_TOKEN/${S3_BUCKET}/g" ${html_file}
