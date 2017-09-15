a6d9417cabe3074975fbeb26d53ea67dd0648b7e
vsnyc <vsnyc@noreply.github.com>
2017-09-15 09:08:11 -0400

    lambda function to save rendered image filenames to dynamodb

---
+++ lambdas/DDRS3RenderedEvent.py
@@ -0,0 +1,24 @@
+import datetime
+import boto3
+import time
+from boto3.dynamodb.conditions import Key, Attr
+
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+
+def lambda_handler(event, context):
+    #print('received event: ' + str(event))
+    s3Key = event['Records'][0]['s3']['object']['key']
+    print('received S3 Key: ' + str(s3Key))
+    file_ts = datetime.datetime.now().isoformat()
+
+    table = dynamodb.Table('ddr_rendered')
+    response = table.put_item(
+        Item={
+            'file_id': 'DUMMY',
+            'file_ts': file_ts, #datetime.datetime.now().isoformat(),
+            'file_name': s3Key,
+            'ttl': int(time.time() + 900)
+        }
+    )
+    # TODO implement
+    return
