bd61e3b960b45cef50612b38e863bc0a98a7f26a
vsnyc <vsnyc@noreply.github.com>
2017-10-27 15:26:43 -0400

    Updated lambda functions

--- lambdas/DDRImageFetch.py
+++ lambdas/DDRImageFetch.py
@@ -10,53 +10,46 @@ dynamodb = boto3.resource('dynamodb')
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-table_name = 'ddr_score'
-pk = 'pk'
+table_name = 'ddr_rendered'
 
 
-def get_last_score():
+def get_last_image():
     table = dynamodb.Table(table_name)
-    db_res = table.query(KeyConditionExpression=Key('score_id').eq('DUMMY'),Limit=1,ScanIndexForward=False)
-    db_res_item = db_res['Items'][0]
-    return {
-        'score': db_res_item['group_total'],
-        'group_avg': db_res_item['group_avg'],
-        'num_people': db_res_item['num_people'],
-        'rek_max': db_res_item['rek_max'],
-        'rek_avg': db_res_item['rek_avg']
-    }
+    db_res = table.query(KeyConditionExpression=Key('file_id').eq('DUMMY'),Limit=1,ScanIndexForward=False)
+    return str(db_res['Items'][0]['file_name'])
 
 
-def get_next_score(last_result):
+def get_next_image(last_result):
     table = dynamodb.Table(table_name)
     last_key = last_result['lastEvaluatedKey']
-    if (not last_key) or (last_key == 'null'):
-        db_res = table.query(KeyConditionExpression=Key('score_id').eq('DUMMY'),Limit=1,ScanIndexForward=False)
+
+    if not last_key:
+        db_res = table.query(KeyConditionExpression=Key('file_id').eq('DUMMY'),Limit=1,ScanIndexForward=False)
     else:
         db_res = table.query(
-            KeyConditionExpression=Key('score_id').eq('DUMMY'),
+            KeyConditionExpression=Key('file_id').eq('DUMMY'),
             Limit=1,
             ScanIndexForward=True,
             ExclusiveStartKey=last_key
         )
 
     if len(db_res['Items']) > 0:
-        score = str(db_res['Items'][0]['group_total'])
+        image = str(db_res['Items'][0]['file_name'])
     else:
-        score = last_result['score']
+        image = last_result['image']
 
     return {
-        'score': score,
+        'image': image,
         'lastEvaluatedKey': db_res.get('LastEvaluatedKey', None)
     }
 
 
 # Lambda function handler method
-def get_score(event, context):
+def get_image(event, context):
     logger.info('got event {}'.format(event))
-    # logger.info('lek: {}'.format(event['queryStringParameters']['lek']))
+    logger.info('lek: {}'.format(event['queryStringParameters']['lek']))
 
-    # Get score
+    # Get image
     table = dynamodb.Table(table_name)
 
     try:
@@ -64,18 +57,16 @@ def get_score(event, context):
     except:
         lek = None
 
-    # last_score = {'score': None, 'lastEvaluatedKey': lek}
-    try:
-        fetched_score = get_last_score()
-    except:
-        fetched_score = {}
+    last_result = {'image': None, 'lastEvaluatedKey': lek}
 
-    #transformed_score = math.tanh(float(fetched_score) / 50.0) * 100.0
-    transformed_score = fetched_score
+    image_result = get_next_image(last_result)
 
-    logger.debug('score is {}'.format(transformed_score))
+    #image_file_name = get_last_image()
+    #tranformed_name = image_file_name.replace(":","_").replace(".jpg","_rendered.png")
 
-    status = transformed_score
+    logger.debug('Image file name on s3 bucket is {}'.format(image_result))
+
+    status = image_result
 
     response = {
         "statusCode": 200,
@@ -85,7 +76,6 @@ def get_score(event, context):
         },
         "body": json.dumps(status, indent=4, cls=DecimalEncoder)
     }
-
     return response
 
 
