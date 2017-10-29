76c23aead32608a950172f3f0856b63179faff7c
vsnyc <vsnyc@noreply.github.com>
2017-10-28 20:18:40 -0400

    Fixed webapp deploy script

--- deploy_webapp.sh
+++ deploy_webapp.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+CF_STACK_ID=$(aws ec2 describe-instances   --instance-id $INSTANCE_ID --region ${REGION} | jq '.[]|.[]|.Instances|.[]|.Tags|.[]|select(.Key == "aws:cloudformation:stack-id")|.Value' | tr -d '"')
 DDR_S3_WEBAPP=$(aws cloudformation describe-stacks --stack ${CF_STACK_ID} --region ${REGION} | jq '.[]|.[]|.Outputs|.[]|select(.OutputKey == "S3DdrWebapp")|.OutputValue' | tr -d '"');echo ${DDR_S3_WEBAPP}
 
 python deploy_webapp.py
